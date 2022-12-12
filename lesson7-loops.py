d049f44e849922b6d6863630b4c2209351130bed
adam <adamgovoni@my.uopeople.edu>
2022-12-11 21:07:06 -0500

    lesson 7 loops

---
+++ lesson7-loops.py
@@ -0,0 +1,19 @@
+'''for eachPass in range(3):
+	print("Wake up!")'''
+
+# iterating from 0 - 9	
+i = 0
+while ((i < 10) == True):
+	print(i)
+	i = i + 1
+
+# iterating from 100 - 0
+i = 100
+while ((i >= 0) == True):
+	print(i)
+	i = i -1
+
+
+	
+
+	
